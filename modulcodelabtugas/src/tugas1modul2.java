3dc5938f6a15fbf1e27e1028896e894e73000feb
fahmyyfa <fahmimajid01@gmail.com>
2024-03-19 19:55:49 +0700

    menambahkan

---
+++ modulcodelabtugas/src/tugas1modul2.java
@@ -0,0 +1,2 @@
+package PACKAGE_NAME;public class tugas1modul2 {
+}
---
+++ modultugascodelab/src/Main2.java
@@ -0,0 +1,19 @@
+class Main1 {
+    public static void main(String[] args) {
+        Tabung tabung = new Tabung();
+        Kubus kubus = new Kubus();
+        Balok balok = new Balok();
+
+        balok.inputNilai();
+        balok.luasPermukaan();
+        balok.volume();
+
+        kubus.inputNilai();
+        kubus.luasPermukaan();
+        kubus.volume();
+
+        tabung.inputNilai();
+        tabung.luasPermukaan();
+        tabung.volume();
+    }
+}
